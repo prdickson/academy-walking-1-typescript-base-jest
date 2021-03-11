fef6d91fcbf94d4e6b14a92380e54af2d368385a
Jackson Jacob <jackson.jacob@cazoo.co.uk>
2021-03-11 15:15:04 +0000

    - Renamed test and added test for fizz

--- src/main/fizzbuzz.ts
+++ src/main/fizzbuzz.ts
@@ -1,4 +1,4 @@
 
 export function fizzbuzz(value: number) {
-
+    return value;
 }
--- src/test/fizzbuzz.test.ts
+++ src/test/fizzbuzz.test.ts
@@ -1,8 +1,13 @@
 import { fizzbuzz } from "../main/fizzbuzz";
 
 describe("fizzbuzz", () => {
-  it("returns the given value", () => {
+  it("returns 1, when given 1", () => {
     const value = fizzbuzz(1);
     expect(value).toEqual(1);
   });
+
+  it('returns fizz, when given 3', ()  => {
+    const value = fizzbuzz(3);
+    expect(value).toEqual('Fizz');
+  })
 });
