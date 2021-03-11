f7556ef35b88db6dabbd6f267a8a270c69c69d92
Paul Dickson <3018673+prdickson@users.noreply.github.com>
2021-03-11 15:47:55 +0000

    Added failing test for fizzbuzz / 15

--- src/test/fizzbuzz.test.ts
+++ src/test/fizzbuzz.test.ts
@@ -6,20 +6,22 @@ describe("fizzbuzz", () => {
     expect(value).toEqual(1);
   });
 
-  it("returns fizz, when given value is divisible by 3", () => {
-    expect(fizzbuzz(3)).toEqual("Fizz");
-    expect(fizzbuzz(6)).toEqual("Fizz");
-    expect(fizzbuzz(9)).toEqual("Fizz");
-  });
+  it.each([[3], [6], [9]])(
+    "returns Fizz, when given value is %d",
+    (n) => {
+      expect(fizzbuzz(n)).toEqual("Fizz");
+    }
+  );
 
-  it.each([[5, 10, 20]], "returns buzz, when given value is divisible by 5", (n) => {
-    expect(fizzbuzz(n)).toEqual("Buzz");
-  });
+  it.each([[5], [10], [20]])(
+    "returns Buzz, when given value is %d",
+    (n) => {
+      expect(fizzbuzz(n)).toEqual("Buzz");
+    }
+  );
 
-  it("returns buzz, when given value is divisible by 5", () => {
-    expect(fizzbuzz(5)).toEqual("Buzz");
-    expect(fizzbuzz(10)).toEqual("Buzz");
-    expect(fizzbuzz(20)).toEqual("Buzz");
+  it("returns FizzBuzz, when given value is divisible by 3 and 5", () => {
+    expect(fizzbuzz(15)).toEqual("FizzBuzz");
   });
 
 });
